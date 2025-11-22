72c2dbee407891362d555ae342c0bb1001258708
nfreitas2000 <nfms.15@icloud.com>
2025-11-22 15:08:32 -0300

    fix: Cors

--- code-with-quarkus/src/main/java/org/acme/Filter/CorsFilter.java
+++ code-with-quarkus/src/main/java/org/acme/Filter/CorsFilter.java
@@ -14,7 +14,7 @@ public class CorsFilter implements ContainerResponseFilter {
             throws IOException {
 
         responseContext.getHeaders().add(
-                "Access-Control-Allow-Origin", "https://axcesstech.vercel.app"
+                "Access-Control-Allow-Origin", "*"
         );
         responseContext.getHeaders().add(
                 "Access-Control-Allow-Credentials", "true"
